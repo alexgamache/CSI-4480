7ceff01321fd2e4e4859650608775062939d0ad0
ollux <agamache@oakland.edu>
2020-12-07 11:11:17 -0500

    Added Framework for Image Encryption/Decryption

--- app/src/main/java/com/example/ollux/csi_4480/MainActivity.java
+++ app/src/main/java/com/example/ollux/csi_4480/MainActivity.java
@@ -4,12 +4,20 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -24,7 +32,12 @@ import com.google.firebase.firestore.EventListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.squareup.picasso.Picasso;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
@@ -55,6 +68,8 @@ public class MainActivity extends AppCompatActivity {
     private byte encryptionKey[] = {9, 115, 51, 86, 105, 4, -31, -23, -68, 88, 17, 20, 3, -105, 119, -53};
     private Cipher cipher, decipher;
     private SecretKeySpec secretKeySpec;
+
+    private static final int SELECT_PICTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -107,9 +122,9 @@ public class MainActivity extends AppCompatActivity {
                         try {
                             for (int i = 0; i < stringMessageArray.length; i++) {
                                 String[] stringKeyValue = stringMessageArray[i].split("=", 2);
-                                System.out.println(Arrays.toString(stringKeyValue));
                                 String[] stringName = stringKeyValue[0].split(";");
-                                stringFinal[1 * i] = (String) stringName[0] + ": " + AESDecryptionMethod(stringKeyValue[1]);
+                                System.out.println(stringKeyValue.length);
+                                stringFinal[i] = (String) stringName[1] + ": " + AESDecryptionMethod(stringKeyValue[1]);
                             }
 
                             listView.setAdapter(new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, stringFinal));
@@ -130,15 +145,93 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Date date = new Date();
-                databaseReference.child(fullName + ";" + Long.toString(date.getTime())).setValue(AESEncryptionMethod(editText.getText().toString()));
+                databaseReference.child(Long.toString(date.getTime()) + ";" + fullName).setValue(AESEncryptionMethod(editText.getText().toString()));
                 editText.setText("");
             }
         });
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == SELECT_PICTURE) {
+                try {
+                    Uri selectedImageUri = data.getData();
+                    final InputStream imageStream;
+                    imageStream = getContentResolver().openInputStream(selectedImageUri);
+                    final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+                    ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                    selectedImage.compress(Bitmap.CompressFormat.PNG, 100, stream);
+                    byte[] byteArray = stream.toByteArray();
+                    selectedImage.recycle();
+                    Date date = new Date();
+                    databaseReference.child(Long.toString(date.getTime()) + ";" + fullName).setValue(AESImageEncryptionMethod(byteArray));
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public Uri getImageUri(Context inContext, Bitmap inImage) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null);
+        return Uri.parse(path);
+    }
+
+    public String getPath(Uri uri) {
+        // just some safety built in
+        if( uri == null ) {
+            return null;
+        }
+        // try to retrieve the image from the media store first
+        // this will only work for images selected from gallery
+        String[] projection = { MediaStore.Images.Media.DATA };
+        Cursor cursor = managedQuery(uri, projection, null, null, null);
+        if( cursor != null ){
+            int column_index = cursor
+                    .getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            String path = cursor.getString(column_index);
+            cursor.close();
+            return path;
+        }
+        // this is our fallback here
+        return uri.getPath();
+    }
+
+
+
     private String AESEncryptionMethod(String string){
 
         byte[] stringByte = string.getBytes();
+        byte[] encryptedByte = new byte[stringByte.length];
+
+        try {
+            cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);
+            encryptedByte = cipher.doFinal(stringByte);
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (BadPaddingException e) {
+            e.printStackTrace();
+        } catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+        }
+
+        String returnString = null;
+
+        try {
+            returnString = new String(encryptedByte, "ISO-8859-1");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return returnString;
+    }
+
+    private String AESImageEncryptionMethod(byte[] stringByte){
+
         byte[] encryptedByte = new byte[stringByte.length];
 
         try {
@@ -181,4 +274,29 @@ public class MainActivity extends AppCompatActivity {
         }
         return decryptedString;
     }
+
+    private String AESImageDecryptionMethod(String string) throws UnsupportedEncodingException {
+        byte[] EncryptedByte = string.getBytes("ISO-8859-1");
+        String decryptedString = string;
+
+        byte[] decryption;
+
+        try {
+            decipher.init(cipher.DECRYPT_MODE, secretKeySpec);
+            decryption = decipher.doFinal(EncryptedByte);
+            decryptedString = new String(decryption);
+            byte[] DecryptedByte = decryptedString.getBytes("ISO-8859-1");
+            ByteArrayInputStream bis = new ByteArrayInputStream(DecryptedByte);
+            Bitmap bp = BitmapFactory.decodeStream(bis);
+
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (BadPaddingException e) {
+            e.printStackTrace();
+        } catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+        }
+        return decryptedString;
+    }
+
 }
